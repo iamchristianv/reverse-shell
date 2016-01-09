26d2dc4d23e4ff53ab27a44490ebcaa38f8b312a
iamchristianv <iamchristianv@gmail.com>
2016-01-09 04:03:08 -0800

    modified connection management with client

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ class Server(object):
     connections = []
     connected = False
 
-    # creates socket, waits for connections from clients, and initiates the prompt for the user
+    # creates a socket, waits for connections from clients, and initiates the main menu for the user
     def start(self):
         self.create_socket()
         self.create_threads()
@@ -27,7 +27,7 @@ class Server(object):
             print("Please try again in a few minutes.\n")
             sys.exit()
 
-    # creates 2 threads to simultaneously accept connections from clients and manage the prompt for the user
+    # creates two threads to simultaneously accept connections from clients and manage input from the user
     def create_threads(self):
         for number in range(2):
             thread = None
@@ -38,20 +38,21 @@ class Server(object):
             thread.daemon = True
             thread.start()
 
-    # accepts connections from clients and adds them to the lists of available connections and addresses
+    # accepts connections from clients and adds them to the lists of connections and addresses
     def accept_connections(self):
         try:
             connection, address = self.sckt.accept()
             self.connections.append(connection)
             self.addresses.append(address)
+            # checks if the output will interrupt the user managing a connection with a client
             if not self.connected:
                 print("\n- connection established with IP address " + address[0])
                 print("- select 1 to see more information on all connections\n")
                 print("Select an option: ")
         except socket.error:
-            print("\nUnable to accept a connection.\n")
+            print("\n- unable to accept a connection\n")
 
-    # manages the prompt for the user based on what he or she entered
+    # manages input from the user for the main menu
     def show_main_menu(self):
         menu_selections = ("\n1) Show Connections", "2) Manage Connection", "3) Quit Program")
         while True:
@@ -68,15 +69,18 @@ class Server(object):
                 break
             else:
                 print("- selection not available")
+        self.close_connections()
 
-    # shows a list of available connections to the user on the prompt
+    # shows a list of available connections to the user
     def show_connections(self):
         print("\n----------- Connections -----------")
         for index, address in enumerate(self.addresses):
             try:
+                # sends a message to each connection in the list to determine which are still available
                 self.connections[index].send("?")
                 self.connections[index].recv(4096)
             except socket.error:
+                # if an exception occurs, then the connection is no longer available
                 self.remove_connection(index)
                 continue
             print("\nID: " + str(index) + (" " * 5) + "IP: " + address[0] + (" " * 5) + "PORT: " + str(address[1]))
@@ -84,6 +88,7 @@ class Server(object):
             print("\n           No Connections")
         print("\n-----------------------------------")
 
+    # lets the user to select a connection to manage from a list of connections
     def select_connection(self):
         if len(self.connections) == 0:
             print("- no connections available")
@@ -98,7 +103,7 @@ class Server(object):
                 self.establish_connection(int(selection))
                 break
 
-    # checks whether the user entered the argument for the 'select' command appropriately
+    # checks whether the user entered an appropriate ID based on available connections
     def error_check_selection(self, argument):
         if not argument.isdigit():
             print("- " + argument + " is not a number")
@@ -113,20 +118,23 @@ class Server(object):
             return True
         return False
 
-    # sends the commands the user entered for the selected client
+    # directly connects the user to the connection selected
     def establish_connection(self, index):
         self.connected = True
         print("\nEnter 'done' to exit the connection\n")
         while True:
+            # commands entered by the user are processed by the client
             command = raw_input(self.addresses[index][0] + "> ")
             if command == "done":
                 break
             elif len(command) > 0:
                 try:
+                    # commands are sent to the client, and the output is returned to the server
                     self.connections[index].send(command)
                     response = str(self.connections[index].recv(4096))
                     print(response)
                 except socket.error:
+                    # if an exception occurs, then the client closed the connection while the user was managing it
                     print("- lost connection")
                     break
         self.connected = False
